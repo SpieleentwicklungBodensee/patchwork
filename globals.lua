fac906fe81e19b0a1dd0c08177ca507a0c10fe5b
Lycea <Lycea@users.noreply.github.com>
2018-11-24 12:24:34 +0100

    Added fix for include

--- globals.lua
+++ globals.lua
@@ -1,6 +1,6 @@
 -- thirdparty
 Gamestate = require("libs/hump/gamestate")
-Assets = require('cargo').init('assets')
+Assets = require('libs/cargo/cargo').init('assets')
 
 -- our stuff
 States = require("states/states")
